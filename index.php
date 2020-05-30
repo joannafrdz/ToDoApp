190aade4ac25166f36cb2f715a589f26afdb6a80
Joanna <joanna.frdz@gmail.com>
2020-05-30 20:36:20 +0400

    Connect to SQL, add and delete : ok

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+$error = "";
+
+$mysqli = new mysqli('localhost', 'root', '', 'todoapp');
+
+if (isset($_POST['submit'])) {
+    $task = $_POST['task'];
+    if (empty($task)) 
+    {
+        $error = 'You must fill in the task';
+    }
+    else
+    {
+        $mysqli->query('INSERT INTO tasks(task) VALUES ("' .$task .'")');
+        header('location: index.php');
+    }
+}
+
+if (isset($_GET['del_task'])) {
+    $id = $_GET['del_task'];
+    $mysqli->query('DELETE FROM tasks WHERE id = ' .$id. ' ');
+    header('location: index.php');
+}
+
+$tasks = $mysqli->query('SELECT * FROM tasks');
+
+?>
+
 <!DOCTYPE HTML>
 <html lang="fr">
 <head>
@@ -12,6 +41,9 @@
     </div>
 
     <form method="post" action="index.php">
+        <?php if (isset($error)) { ?>
+            <p><?php echo $error; ?></p>
+        <?php } ?>
         <input type="text" name="task" class="task_input">
         <button type="submit" class="add_btn" name="submit">Add task</button>
     </form>
@@ -20,19 +52,23 @@
         <thead>
             <tr>
             <th>N</th>
-            <th>Task</th>
+            <th>Tasks</th>
             <th>Action</th>
             </tr>
         </thead>
 
         <tbody>
-            <tr>
-                <td>1</td>
-                <td class="task">First task placeholder</td>
-                <td class="delete">
-                    <a href="">x</a>
-                </td>
-            </tr>
+            <?php $i = 1; while ($row = $tasks->fetch_array()) { ?>
+                <tr>
+                    <td><?php echo $i; ?></td>
+                    <td class="task"><?php echo $row['task']; ?></td>
+                    <td class="delete">
+                        <a href="index.php?del_task=<?php echo $row['id']; ?>">x</a>
+                    </td>
+                </tr>
+
+            <?php $i++; } ?>
+
         </tbody>
     </table>
 
