d74fd426307436064ae0bb2df7b85b46ef39e161
Joanna <joanna.frdz@gmail.com>
2020-05-30 19:44:05 +0400

    Create form

--- index.php
+++ index.php
@@ -13,8 +13,28 @@
 
     <form method="post" action="index.php">
         <input type="text" name="task" class="task_input">
-        <button type="submit" class="task_btn" name="submit">Add task</button>
+        <button type="submit" class="add_btn" name="submit">Add task</button>
     </form>
+
+    <table>
+        <thead>
+            <tr>
+            <th>N</th>
+            <th>Task</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td class="task">First task placeholder</td>
+                <td class="delete">
+                    <a href="">x</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 </body>
 </html>
