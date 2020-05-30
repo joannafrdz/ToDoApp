24bfd066b5696eb66e5b395975f5cf4fdc6297d1
Joanna <joanna.frdz@gmail.com>
2020-05-30 19:19:48 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <title>ToDo App</title>
+    <link rel="stylesheet" href="style.css" type="text/css" >
+</head>
+<body>
+
+    <div class="heading">
+        <h2>ToDo App</h2>
+    </div>
+
+    <form method="post" action="index.php">
+        <input type="text" name="task" class="task_input">
+        <button type="submit" class="task_btn" name="submit">Add task</button>
+    </form>
+
+</body>
+</html>
